e3769799ab6963298964635f48813272c58cb7d8
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-11-13 03:46:49 -0800

    Convert class component to functional component

--- react-classComponent-app/src/components/BookList.js
+++ react-classComponent-app/src/components/BookList.js
@@ -1,18 +1,24 @@
 import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+  const { books } = useContext(BookContext);
   return (
     <div
       className="book-list"
       style={{ background: theme.bg, color: theme.syntax }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map((book) => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
